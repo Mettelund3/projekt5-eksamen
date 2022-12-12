44fcf1f6f65babbc0932e081ade88f5d283386c5
Maximilian90 <105641605+Maximilian90@users.noreply.github.com>
2022-12-12 22:35:13 +0100

    Chart

--- energiforbrug.js
+++ energiforbrug.js
@@ -20,3 +20,115 @@
      }
    } 
  });
+
+ const labels =  ['Man. 05.12.2022 ', 'Tir. 06.12.2022', 'Ons. 07.12.2022', 'Tors. 08.12.2022', 'Fre. 09.12.2022', 'Lør. 10.12.2022', 'Søn. 11.12.2022']
+ const data = {
+     labels: labels,
+     datasets: [{
+         label: 'Energiforbrug i kWh',
+         data: [65, 59, 80, 81, 56, 55, 40],
+         backgroundColor: [
+             'rgba(54, 162, 235, 0.2)',
+         ],
+         borderColor: [
+             'rgba(54, 162, 235, 1)'
+         ],
+         borderWidth: 1,
+         borderRadius: 20,
+         barPercentage: 0.5,
+     }]
+     };
+ 
+     
+ /*config block*/
+ 
+     const config = {
+         type: 'bar',
+         data,
+         options: {
+             maintainAspectRatio: false, /*take out default sizing*/
+             scales: {
+                 y: {
+                 beginAtZero: true
+                 }
+             }
+ 
+         }
+     };   
+ 
+     const config2 = {
+         type: 'pie',
+         data,
+         options: {
+             maintainAspectRatio: false, /*take out default sizing*/
+             
+         }
+     };
+     
+     
+ 
+ /*render block*/
+     let myChart = new Chart (
+         document.getElementById('myChart'),
+         config
+     );
+ 
+ 
+ 
+     function chartType(type) {
+         //destroy Chart
+         myChart.destroy ();
+             if (type === 'bar') {
+                 myChart = new Chart (
+                 document.getElementById('myChart'),
+                 config
+                 );   
+             }        
+         
+             
+             if (type === 'pie') {
+                 myChart = new Chart (
+                 document.getElementById('myChart'),
+                 config2
+                 );   
+             }
+         
+     }
+ 
+ 
+ 
+ /*For each*/ 
+ var forbrug = [
+     {
+         Date: "05.12.2022",
+         kwh: "65 kwh"
+     },
+     {
+         Date: "06.12.2022",
+         kwh: "59 kwh"
+     },
+     {
+         Date: "07.12.2022",
+         kwh: "80 kwh"
+     },
+     {
+         Date: "08.12.2022",
+         kwh: "81 kwh"
+     },
+     {
+         Date: "09.12.2022",
+         kwh: "56 kwh"
+     },
+     {
+         Date: "10.12.2022",
+         kwh: "55 kwh"
+     },
+     {
+         Date: "11.12.2022",
+         kwh: "40 kwh"
+     }
+ ];
+ 
+ forbrug.forEach(function(forbrug){
+         console.log(forbrug)
+     });
